10db703c92392a7c72810fa9eff8e27cdb5c67d6
ssodboeva <radnaevas87@gmail.com>
2024-10-05 13:01:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for coat in numbers:
-    if coat == 1:
+for count in numbers:
+    if count == 1:
         continue
     is_prime = True
-    for i in range (2, coat):
-        if coat % i == 0:
+    for i in range (2, count):
+        if count % i == 0:
             is_prime = False
     if is_prime:
-        primes.append(coat)
+        primes.append(count)
     else:
-        not_primes.append(coat)
+        not_primes.append(count)
 print (primes)
 print (not_primes)
